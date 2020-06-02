f433582aad8bbf14b5220d0b9ae7207a370a515b
Steven <swm9220@gmail.com>
2020-06-02 14:50:35 -0700

    up to function basics 2

--- function_basics2.py
+++ function_basics2.py
@@ -1,19 +1,44 @@
 # Countdown - Create a function that accepts a number as an input. Return a new list that counts down by one, from the number (as the 0th element) down to 0 (as the last element).
 # Example: countdown(5) should return [5,4,3,2,1,0]
 
+def countdown():
+    list = []
+    for i in range(5,-1,-1):
+        list.append(i)
+    return list
+print(countdown())
 
 # Print and Return - Create a function that will receive a list with two numbers. Print the first value and return the second.
 # Example: print_and_return([1,2]) should print 1 and return 2
-
+list1 = [1,2]
+def print_and_return(list1):
+    print(list1[0])
+    return list1[1]
+print(print_and_return(list1))
 
 # First Plus Length - Create a function that accepts a list and returns the sum of the first value in the list plus the list's length.
 # Example: first_plus_length([1,2,3,4,5]) should return 6 (first value: 1 + length: 5)
-
-
+list2 = [1,2,3,4,5]
+def first_plus_length(list2):
+    return list2[0] + len(list2)
+print(first_plus_length(list2))
 
 # Values Greater than Second - Write a function that accepts a list and creates a new list containing only the values from the original list that are greater than its 2nd value. Print how many values this is and then return the new list. If the list has less than 2 elements, have the function return False
 # Example: values_greater_than_second([5,2,3,2,1,4]) should print 3 and return [5,3,4]
 # Example: values_greater_than_second([3]) should return False
+def values_greater_than_second(list3):
+    new_list = []
+    i = 0
+    count = list3[i]
+    while i < len(list3):
+        if count > list3[1]:
+            new_list.append(count)
+            count+=1
+        print(new_list)
+    return len(new_list)
+list3 = values_greater_than_second([5,2,3,2,1,4])
+print(list3)
+
 
 
 # This Length, That Value - Write a function that accepts two integers as parameters: size and value. The function should create and return a list whose length is equal to the given size, and whose values are all the given value.
