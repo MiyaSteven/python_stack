3820ad85a5ae5f3dd833ad67e192eae7d0619eed
Steven <swm9220@gmail.com>
2020-06-03 17:54:51 -0700

    added bank accounts

--- attributes/user.py
+++ attributes/user.py
@@ -1,17 +1,44 @@
+class BankAccount:
+    def __init__(self, interest_rate=0.1, account_balance=0):
+        self.account_balance = account_balance
+        self.interest_rate = interest_rate
+    def deposit(self, amount):
+        self.account_balance += amount
+        return self
+    def withdraw(self, amount):
+        self.account_balance -= amount
+        return self
+    def display_account_info(self):
+        print(self.account_balance)
+        return self
+    def yield_interest(self):
+        self.account_balance += self.account_balance * self.interest_rate 
+        return self
+
+StevensAccount = BankAccount(.2, 1000)
+JoesAccount = BankAccount(.15, 2000)
+
+StevensAccount.deposit(500).deposit(500).deposit(750).yield_interest().display_account_info()
+JoesAccount.deposit(500).deposit(1000).withdraw(100).withdraw(199).withdraw(201).withdraw(100).yield_interest().display_account_info()
+
 class User:		# declare a class and give it name User
     def __init__(self, name, email):
         self.name = name
         self.email = email
-        self.account_balance = 0
+        self.account = BankAccount(interest_rate=.02, account_balance=0)
+
     def make_withdraw(self, amount):
         self.account_balance -= amount
         return self
+
     def make_deposit(self, amount):
         self.account_balance = self.account_balance + amount
         return self
+
     def display_user_balance(self, account_balance):
         print(self.account_balance)
         return self
+
     def transfer_funds(self, recipient, amount):
         pass
 
