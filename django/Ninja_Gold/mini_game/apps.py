da1313b54ed552390555e8ba85cb8693f2a0f768
Steven <swm9220@gmail.com>
2020-06-09 13:55:10 -0700

    added settings and gameover files

---
+++ django/Ninja_Gold/mini_game/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MiniGameConfig(AppConfig):
+    name = 'mini_game'
---
+++ django/Ninja_Gold/mini_game/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, HttpResponse, redirect
+from time import strftime
+import random
+
+def index(request):
+    return render(request, 'index.html')
+
+def process_money(request):
+    if 'count' in request.session:
+        if request.POST['building'] == 'farm':
+            score = random.randint(10,20)
+        elif request.POST['building'] == 'cave':
+            score = random.randint(5,10)
+        elif request.POST['building'] == 'house':
+            score = random.randint(2,5)
+        elif request.POST['building'] == 'casino':
+            score = random.randint(-50,50)
+        request.session['count'] += score
+        if score > 0:
+            request.session['box'].insert(0,{'class': 'green', 'log': f"You earned {score} golds from the {request.POST['building']}! {strftime('%Y/%m/%d %I:%M:%S %p')}"})
+        else:
+            request.session['box'].insert(0,{'class': 'red', 'log': f"You lost {-1*score} golds at the {request.POST['building']}... Ouch!!! {strftime('%Y/%m/%d %I:%M:%S %p')}"})
+    else:
+        request.session['count'] = 0
+        request.session['box'] = []
+    return redirect('/')
+
+def reset(request):
+    request.session['count'] = 0
+    request.session['box'] = []
+    return redirect('/')
+
+def user_settings(request):
+    request.session['turns'] = 0
+    request.session['goals'] = 0
+    return render(request, 'users_rules.html')
+
+def change_settings(request):
+    request.session['turns'] = request.POST['max_turns']
+    request.session['goals'] = request.POST['gold_goal']
+    return redirect('/')
+
+# Ron , Casey, Keith, Steven, Daniel, Jerome
