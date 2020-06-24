9d8a5944fc693b06dae45b68626665709254e485
Steven <swm9220@gmail.com>
2020-06-23 18:53:22 -0700

    login and reg

---
+++ django/donta_rpg/main/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import User
+from datetime import datetime
+import bcrypt
+
+def index(request):
+    return render(request, 'login.html')
+
+def register(request):
+    errors = User.objects.register_validator(request.POST)
+    hashed = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
+    if len(errors) > 0:
+        for error in errors.values():
+            messages.error(request, error)
+        return redirect('/')
+    this_user = User.objects.create(
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        email=request.POST['email'],
+        password=hashed,
+        confirm_password=hashed
+    )
+    request.session['user_id'] = this_user.id
+    return redirect('/character')
+
+def login(request):
+    users = User.objects.filter(email=request.POST['email'])
+    if users:
+        log_in_user = users[0]
+        if  bcrypt.checkpw(request.POST['password'].encode(), log_in_user.password.encode()):
+            request.session['user_id'] = log_in_user.id
+            return redirect('/character')
+    messages.error(request, "Email and/or Password not found")
+    return redirect('/')
+    
